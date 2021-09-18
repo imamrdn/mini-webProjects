0a9f39194b4e32e44a3abb9f6058a56c77867b5f
Imam Ramadhan <imamrmdn99@gmail.com>
2021-09-18 07:50:02 +0700

    add weatherApi

--- weatherApp-2/script.js
+++ weatherApp-2/script.js
@@ -2,3 +2,23 @@ const wrapper = document.querySelector(".wrapper"),
     inputPart = wrapper.querySelector(".input-part"),
     infoTxt = inputPart.querySelector(".info-txt"),
     inputField = inputPart.querySelector("input");
+
+inputField.addEventListener("keyup", e => {
+    //if user pressed enter btn and input value is not empty
+    if (e.key == "Enter" && inputField.value != "") {
+        requestApi(inputField.value);
+    }
+});
+
+function requestApi(city) {
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2501969fb7ff4df90a8dd500e3ad7522`;
+    infoTxt.innerHTML = "Getting weather details...";
+    infoTxt.classList.add("pending");
+    //getting api response and returning it with parsing into js and in another
+    //then function calling weatherDetails function with passing api result as an argument
+    fetch(api).then(response => response.json()).then(result => weatherDetails(result));
+}
+
+function weatherDetails(info) {
+    console.log(info);
+}
