684d0bf7c2b01049a88c46551878cf17f900635c
Imam Ramadhan <imamrmdn99@gmail.com>
2021-09-24 10:20:33 +0700

    smooth scroll with jQuery

---
+++ smooth-scroll/main.js
@@ -0,0 +1,14 @@
+//option 2 - jQuery smooth scroll
+// $('.navbar a').on('click', function(e) {
+//     if (this.hash !== '') {
+//         e.preventDefault();
+
+//         const hash = this.hash;
+
+//         $('html, body').animate({
+//             scrollTop: $(hash).offset().top
+//         }, 800);
+//     }
+// });
+
+// option 3 - smooth scroll script
