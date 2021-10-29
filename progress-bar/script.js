413eb3b1e7539f94f75de83b64d25b2b5a5f343e
Imam Ramadhan <imamrmdn99@gmail.com>
2021-10-29 10:02:10 +0700

    add nextBtns

--- progress-bar/script.js
+++ progress-bar/script.js
@@ -1,3 +1,17 @@
 const prevBtns = document.querySelectorAll(".btn-prev");
 const nextBtns = document.querySelectorAll(".btn-next");
 const progress = document.getElementById("progress");
+const formSteps = document.querySelectorAll(".form-step");
+
+let formStepsNum = 0;
+
+nextBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        formStepsNum++;
+        updateFormSteps();
+    });
+});
+
+function updateFormSteps() {
+    formSteps[formStepsNum].classList.add("form-step-active");
+}
